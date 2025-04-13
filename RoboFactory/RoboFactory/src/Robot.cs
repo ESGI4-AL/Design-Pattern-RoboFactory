3b98d5f5b11a8d592526e6b900d5920f62fe517f
denisadudas <dudasdenisaandrea@yahoo.ro>
2025-04-13 18:35:46 +0200

    added robots and its components

---
+++ RoboFactory/RoboFactory/src/Robot.cs
@@ -0,0 +1,37 @@
+﻿namespace RoboFactory;
+
+public class Robot(
+    string name,
+    int quantity,
+    string coreName,
+    string generatorName,
+    string armsName,
+    string legsName,
+    string systemName)
+{
+    public string Name { get; set; } = name;
+    public int Quantity { get; set; } = quantity;
+
+    public string CoreName { get; set; } = coreName;
+    public string GeneratorName { get; set; } = generatorName;
+    public string ArmsName { get; set; } = armsName;
+    public string LegsName { get; set; } = legsName;
+    public string SystemName { get; set; } = systemName;
+
+    public Dictionary<string, int> GetRequiredPieces()
+    {
+        return new Dictionary<string, int>
+        {
+            { CoreName, 1 * Quantity },
+            { GeneratorName, 1 * Quantity },
+            { ArmsName, 1 * Quantity },
+            { LegsName, 1 * Quantity },
+            { SystemName, 1 * Quantity }
+        };
+    }
+    
+    public override string ToString()
+    {
+        return $"{Quantity} - {Name}";
+    }
+}
