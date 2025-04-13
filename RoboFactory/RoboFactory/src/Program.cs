e9ec6b1bd15f1500d6fb5a26f661b44667caf9b4
denisadudas <dudasdenisaandrea@yahoo.ro>
2025-04-13 18:37:59 +0200

    updated the program file accordingly

--- RoboFactory/RoboFactory/src/Program.cs
+++ RoboFactory/RoboFactory/src/Program.cs
@@ -1,2 +1,45 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using RoboFactory.Utils;
+
+namespace RoboFactory
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factoryInventory = new FactoryInventory();
+            var commandManager = new CommandManager(factoryInventory);
+            
+            DisplayMessages.DisplayWelcomeMessage();
+            Console.WriteLine("Enter your command :");
+
+            while (true)
+            {
+                Console.Write("> ");
+                string? input = Console.ReadLine();
+                
+                if (string.IsNullOrEmpty(input))
+                    continue;
+
+                string command = input.Split(' ')[0];
+                CommandType commandType = CommandTypeUtils.FromStringToEnum(command);
+                
+                if (commandType == CommandType.Exit)
+                {
+                    Console.WriteLine("Exiting the program...");
+                    break;
+                }
+                
+                if (commandType == CommandType.Help)
+                {
+                    DisplayMessages.DisplayHelpMenu();
+                    continue;
+                }
+                
+                string result = commandManager.ProcessCommands(input);
+                Console.WriteLine(result);
+            }
+            
+            Console.WriteLine("Good bye!");
+        }
+    }
+}
